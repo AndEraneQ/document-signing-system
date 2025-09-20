7dd015d5a79e8a19dee6df7415842e519c9bb810
Piotr Trojan <trojanpiotrek@interia.pl>
2025-09-20 13:49:59 +0200

    Create server directory

---
+++ server/src/main/java/com/trojan/document_signing_system/DocumentSigningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trojan.document_signing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSigningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSigningSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/trojan/document_signing_system/DocumentSigningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trojan.document_signing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSigningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
