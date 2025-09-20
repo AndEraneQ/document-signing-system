5221a37ad0469e3e63ed8337703f02fdbe856cbd
anderan <129384174+AndEraneQ@users.noreply.github.com>
2025-09-20 14:06:55 +0200

    Set up Authorization and Api Gateway microservices (#3)

---
+++ server/api-gateway/src/test/java/com/trojan/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trojan.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
