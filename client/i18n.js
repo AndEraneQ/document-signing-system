91b40af33862a5c1d53eda716eba073c60549c49
Piotr Trojan <trojanpiotrek@interia.pl>
2025-10-18 11:20:47 +0200

    configure tailwind

--- client/i18n.js
+++ client/i18n.js
@@ -2,15 +2,18 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-i18n.use('en')
+import enTranslation from './src/config/locales/en/translation.json'
+import plTranslation from './src/config/locales/pl/translation.json'
+
+i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: 'en',
         debug: false,
         interpolation: { escapeValue: false },
         resources: {
-            en: { translation: require('./locales/en/translation.json') },
-            pl: { translation: require('./locales/pl/translation.json') },
+            en: { translation: enTranslation },
+            pl: { translation: plTranslation },
         },
     })
 
