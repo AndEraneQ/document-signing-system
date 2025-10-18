89d5b8306013603cf46704eb5a8fb5e651e0003f
Piotr Trojan <trojanpiotrek@interia.pl>
2025-10-18 09:47:07 +0200

    Configure translation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './config/router'
+import { router } from './config/router/router'
+import '../i18n';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
