bb0701b0c55b46dc0850a38fd31abf50fafe66ce
anderan <129384174+AndEraneQ@users.noreply.github.com>
2025-11-13 19:01:59 +0100

    Change architecture to monolith (#9)

---
+++ server/src/test/kotlin/com/example/document_signing_system/DocumentSigningSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.document_signing_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocumentSigningSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
